cdfd742c5198a2e590471f1b4be56b77ac07756f
AbhishekGusain1289 <94604044+AbhishekGusain1289@users.noreply.github.com>
2024-05-05 20:44:51 +0530

    Update app.py
    Updated the algorithm

--- app.py
+++ app.py
@@ -4,37 +4,45 @@ from sklearn.metrics.pairwise import cosine_similarity
 from joblib import load
 import pandas as pd
 
-app = Flask(__name__)
+app = Flask(_name_)
 
 # Load the pre-trained model
-tfidf_vectorizer = load('tfidf_vectorizer.joblib')
-tfidf_matrix = load('tfidf_matrix.joblib')
+# tfidf_vectorizer = load('tfidf_vectorizer.joblib')
+# tfidf_matrix = load('tfidf_matrix.joblib')
 
 @app.route('/recommend', methods=['POST'])
 def recommend_dish():
     try:
-        print("Inside the function")
+        def filter_ingredients(df, filter_list):
+            filtered_df = df[df['ingredients'].apply(lambda x: all(ingredient in x for ingredient in filter_list))]
+            return filtered_df
+
         data = request.get_json()
         ingredients = data['ingredients']
-        df1=pd.read_csv("cleaned_file-1.csv",index_col="index")
-        df2=pd.read_csv("cleaned_file-2.csv",index_col="index")
-        df3=pd.read_csv("cleaned_file-3.csv",index_col="index")
-        df=pd.concat([df1,df2,df3])
+        # Lowercase the elements of essentials list
+        essentials = [essential.lower() for essential in data.get("essentials", [])]
+
+        df = pd.read_csv("cleaned_file.csv", index_col="index")
+
+        result = filter_ingredients(df.copy(), essentials)
+
+        tfidf_vectorizer2 = TfidfVectorizer(stop_words='english')
+        tfidf_matrix2 = tfidf_vectorizer2.fit_transform(result['ingredients'])
 
         # Use the model for recommendation
-        user_idf = tfidf_vectorizer.transform([ingredients])
-        sim_ing = cosine_similarity(user_idf, tfidf_matrix)
-        li=sorted(list(enumerate(sim_ing[0])),reverse=True,key=lambda x:x[1])[0:5]
-        li
+        user_idf = tfidf_vectorizer2.transform([ingredients])
+        sim_ing = cosine_similarity(user_idf, tfidf_matrix2)
+        li = sorted(list(enumerate(sim_ing[0])), reverse=True, key=lambda x: x[1])[0:5]
         indices = [index for index, _ in li]
-        newdf=df.loc[indices]
-        json=newdf.to_json(orient='records')
-        return json
+        newdf = result.iloc[indices]
+        json_data = newdf.to_json(orient='records')
+
+        return json_data
 
     except Exception as e:
         # Log the exception details
         print(f"Error: {e}")
         return jsonify({'error': 'Internal Server Error'}), 500
 
-if __name__ == '__main__':
-    app.run(debug=False)
+if _name_ == '_main_':
+    app.run(debug=True, host="0.0.0.0", port=5000)
